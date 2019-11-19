dd9dcf493b91641afe178ec41a2f2fb8f64270b5
Aluno07 <Aluno07@DESKTOP-SLB1SQ8>
2019-11-18 22:28:27 -0200

    Lista de tarefas com banco de dados (FINALMENTE FINALIZADA!!!!)

--- src/mysql/ii/MySQLII.java
+++ src/mysql/ii/MySQLII.java
@@ -13,6 +13,7 @@ public class MySQLII {
     static Scanner sc = new Scanner(System.in);
     static int linhas;
     static Usuario usuarioLogado = new Usuario();
+    static String keep;
 
     public static void main(String[] args) {
         //Classes
@@ -21,7 +22,6 @@ public class MySQLII {
         String email;
         String senha;
         String op;
-        String keep;
 
         boolean running = true;
 
@@ -42,11 +42,29 @@ public class MySQLII {
                     System.out.println("||==================||");
                     System.out.println("|| MENU DE CADASTRO ||");
                     System.out.println("||==================||");
-                    System.out.print("Insira o novo email: ");
-                    email = sc.nextLine();
 
-                    System.out.print("Insira a nova senha: ");
-                    senha = sc.nextLine();
+                    boolean verificarEmail = true;
+                    do {
+                        System.out.print("Insira o novo email: ");
+                        email = sc.nextLine();
+                        if (email.contains("@")) {
+                            verificarEmail = false;
+                        } else {
+                            System.out.println("o email deve conter @");
+                        }
+                    } while (verificarEmail);
+
+                    boolean verificarSenha;
+                    do {
+                        System.out.print("Insira a nova senha: ");
+                        senha = sc.nextLine();
+                        if (senha.length() < 6 || senha.length() > 15) {
+                            System.out.println("A senha deve conter no mínimo 6 ou no máximo 16 caracteres");
+                            verificarSenha = false;
+                        } else {
+                            verificarSenha = true;
+                        }
+                    } while (!verificarSenha);
 
                     pegarId(email, senha);
 
@@ -54,6 +72,9 @@ public class MySQLII {
                     usuarioLogado.setSenha(senha);
 
                     inserirUsuario(email, senha);
+
+                    System.out.println("Aperte enter para continuar");
+                    keep = sc.nextLine();
                     break;
 
                 case "2":
@@ -67,17 +88,17 @@ public class MySQLII {
                     System.out.print("Insira a senha: ");
                     senha = sc.nextLine();
 
+                    pegarId(email, senha);
                     logarUsuario(email, senha);
                     break;
 
                 case "3":
                     running = false;
                     System.out.println("Finalizando o programa...");
+                    System.out.println("Aperte enter para continuar");
+                    keep = sc.nextLine();
                     break;
             }
-
-            System.out.println("Aperte enter para continuar");
-            keep = sc.nextLine();
         }
 
     }
@@ -121,13 +142,24 @@ public class MySQLII {
             stmnt.setString(1, email);
             stmnt.setString(2, senha);
 
-            linhas = stmnt.executeUpdate();
+            ResultSet rs = stmnt.executeQuery();
 
-            if (linhas > 0) {
-                System.out.println("Usuário logado com sucesso!");
+            while (rs.next()) {
+                String emailc = rs.getString("des_email");
+                String senhac = rs.getString("enc_senha");
 
-            } else {
-                System.out.println("Usuário e/ou senha incorretos!");
+                if (emailc.equals(email) && senhac.equals(senha)) {
+                    System.out.println("Usuário logado com sucesso");
+                    
+                    System.out.println("Aperte enter para continuar");
+                    keep = sc.nextLine();
+                    
+                    boolean entrar = true;
+                    
+                    segundoMenu(entrar);
+                } else {
+                    System.out.println("Email/senha incorretos");
+                }
             }
 
         } catch (Exception e) {
@@ -135,7 +167,7 @@ public class MySQLII {
         }
     }
 
-    static void listarTarefas() {
+    static void listarTarefas(int id) {
         try {
             Driver driver = new Driver();
 
@@ -143,32 +175,28 @@ public class MySQLII {
 
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "");
 
-            PreparedStatement stmnt = conn.prepareStatement("select * from lista_de_tarefas.tb_tarefas");
+            PreparedStatement stmnt = conn.prepareStatement("select * from lista_de_tarefas.tb_tarefas where id_usuario = ?");
+            
+            stmnt.setInt(1, id);
 
             ResultSet rs = stmnt.executeQuery();
 
             while (rs.next()) {
-                int id = rs.getInt("id_tarefa");
+                int idTarefa = rs.getInt("id_tarefa");
                 String assunto = rs.getString("des_assunto");
                 boolean finalizada = rs.getBoolean("sta_finalizada");
 
-                if (finalizada) {
-                    System.out.print("finalizada");
-                } else {
-                    System.out.print("não finalizada");
-                }
-
-                System.out.println("Tarefa: " + id);
+                System.out.println("Tarefa: " + idTarefa);
                 System.out.println("Assunto: " + assunto);
                 System.out.println("Finalizada: " + finalizada);
             }
 
         } catch (Exception e) {
-
+            System.out.println("Algum erro aconteceu!");
         }
     }
 
-    static void listarTarefasFinalizadas() {
+    static void listarTarefasFinalizadas(int id) {
         try {
             Driver driver = new Driver();
 
@@ -176,31 +204,27 @@ public class MySQLII {
 
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "");
 
-            PreparedStatement stmnt = conn.prepareStatement("select * from lista_de_tarefas.tb_tarefas where sta_finalizada = 1");
+            PreparedStatement stmnt = conn.prepareStatement("select * from lista_de_tarefas.tb_tarefas where sta_finalizada = 1 and id_usuario = ?");
+            
+            stmnt.setInt(1, id);
 
             ResultSet rs = stmnt.executeQuery();
 
             while (rs.next()) {
-                int id = rs.getInt("id_tarefa");
+                int idTarefa = rs.getInt("id_tarefa");
                 String assunto = rs.getString("des_assunto");
                 boolean finalizada = rs.getBoolean("sta_finalizada");
 
-                if (finalizada) {
-                    System.out.print("finalizada");
-                } else {
-                    System.out.print("não finalizada");
-                }
-
-                System.out.println("Tarefa: " + id);
+                System.out.println("Tarefa: " + idTarefa);
                 System.out.println("Assunto: " + assunto);
                 System.out.println("Finalizada: " + finalizada);
             }
         } catch (Exception e) {
-
+            System.out.println("Algum erro aconteceu!");
         }
     }
 
-    static void listarTarefasNaoFinalizadas() {
+    static void listarTarefasNaoFinalizadas(int id) {
         try {
             Driver driver = new Driver();
 
@@ -208,31 +232,27 @@ public class MySQLII {
 
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "");
 
-            PreparedStatement stmnt = conn.prepareStatement("select * from lista_de_tarefas.tb_tarefas where sta_finalizada = 0");
+            PreparedStatement stmnt = conn.prepareStatement("select * from lista_de_tarefas.tb_tarefas where sta_finalizada = 0 and id_usuario = ?");
+            
+            stmnt.setInt(1, id);
 
             ResultSet rs = stmnt.executeQuery();
 
             while (rs.next()) {
-                int id = rs.getInt("id_tarefa");
+                int idTarefa = rs.getInt("id_tarefa");
                 String assunto = rs.getString("des_assunto");
                 boolean finalizada = rs.getBoolean("sta_finalizada");
 
-                if (finalizada) {
-                    System.out.print("finalizada");
-                } else {
-                    System.out.print("não finalizada");
-                }
-
-                System.out.println("Tarefa: " + id);
+                System.out.println("Tarefa: " + idTarefa);
                 System.out.println("Assunto: " + assunto);
                 System.out.println("Finalizada: " + finalizada);
             }
         } catch (Exception e) {
-
+            System.out.println("Algum erro aconteceu!");
         }
     }
 
-    static void adicionarTarefa(String assunto) {
+    static void adicionarTarefa(String assunto, int id) {
         try {
             Driver driver = new Driver();
 
@@ -240,19 +260,20 @@ public class MySQLII {
 
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "");
 
-            PreparedStatement stmnt = conn.prepareStatement("insert into lista_de_tarefas.tb_tarefas (des_assunto) values (?)");
+            PreparedStatement stmnt = conn.prepareStatement("insert into lista_de_tarefas.tb_tarefas (des_assunto, id_usuario) values (?, ?)");
 
             stmnt.setString(1, assunto);
+            stmnt.setInt(2, id);
 
             linhas = stmnt.executeUpdate();
 
             if (linhas > 0) {
                 System.out.println("Tarefa adicionada com sucesso!");
             } else {
-                System.out.println("Tarefa não adicionada, algum erro aconteceu!");
+                System.out.println("Tarefa não adicionada!");
             }
         } catch (Exception e) {
-
+            System.out.println("Algum erro aconteceu!");
         }
     }
 
@@ -273,11 +294,11 @@ public class MySQLII {
             if (linhas > 0) {
                 System.out.println("Tarefa finalizada com sucesso!");
             } else {
-                System.out.println("Tarefa não finalizada. Algum erro aconteceu!");
+                System.out.println("Tarefa não finalizada.");
             }
 
         } catch (Exception e) {
-
+            System.out.println("Algum erro aconteceu!");
         }
     }
 
@@ -302,7 +323,7 @@ public class MySQLII {
             }
 
         } catch (Exception e) {
-
+            System.out.println("Algum erro aconteceu!");
         }
     }
 
@@ -315,19 +336,20 @@ public class MySQLII {
 
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "");
 
-            PreparedStatement stmnt = conn.prepareStatement("select id_usuario from tb_usuario where des_email = ? and enc_senha = ?");
+            PreparedStatement stmnt = conn.prepareStatement("select id_usuario from lista_de_tarefas.tb_usuario where des_email = ? and enc_senha = ?");
 
             stmnt.setString(1, email);
             stmnt.setString(2, senha);
 
             ResultSet rs = stmnt.executeQuery();
 
-            int id = rs.getInt("id_usuario");
+            while (rs.next()) {
+                int id = rs.getInt("id_usuario");
 
-            usuarioLogado.setId(id);
-
+                usuarioLogado.setId(id);
+            }
         } catch (Exception e) {
-
+            System.out.println("Algum erro aconteceu!");
         }
 
     }
@@ -340,12 +362,12 @@ public class MySQLII {
         //Variáveis
         String op;
         String assunto;
-        String keep;
 
         int idTarefa;
         boolean finalizada = false;
 
         while (entrar) {
+            System.out.println("");
             System.out.println("||=================||");
             System.out.println("|| MENU DO USUÁRIO ||");
             System.out.println("||=================||");
@@ -356,6 +378,7 @@ public class MySQLII {
             System.out.println("[5] Finalizar tarefas");
             System.out.println("[6] Remover uma tarefa");
             System.out.println("[7] Fazer logout");
+            System.out.print("Selecione uma opção:");
             op = sc.nextLine();
 
             switch (op) {
@@ -364,7 +387,8 @@ public class MySQLII {
                     System.out.println("||================||");
                     System.out.println("|| LISTAR TAREFAS ||");
                     System.out.println("||================||");
-                    listarTarefas();
+                    int id = usuarioLogado.getId();
+                    listarTarefas(id);
                     break;
 
                 case "2":
@@ -372,7 +396,8 @@ public class MySQLII {
                     System.out.println("||============================||");
                     System.out.println("|| LISTAR TAREFAS FINALIZADAS ||");
                     System.out.println("||============================||");
-                    listarTarefasFinalizadas();
+                    id = usuarioLogado.getId();
+                    listarTarefasFinalizadas(id);
                     break;
 
                 case "3":
@@ -380,7 +405,8 @@ public class MySQLII {
                     System.out.println("||================================||");
                     System.out.println("|| LISTAR TAREFAS NÃO FINALIZADAS ||");
                     System.out.println("||================================||");
-                    listarTarefasNaoFinalizadas();
+                    id = usuarioLogado.getId();
+                    listarTarefasNaoFinalizadas(id);
                     break;
 
                 case "4":
@@ -396,7 +422,8 @@ public class MySQLII {
                     tarefa.setFinalizada(finalizada);
 
                     if (tarefas.add(tarefa)) {
-                        System.out.println("Tarefa asicionada com sucesso!");
+                        id = usuarioLogado.getId();
+                        adicionarTarefa(assunto, id);
                     } else {
                         System.out.println("Tarefa não adicionada");
                     }
@@ -410,21 +437,11 @@ public class MySQLII {
                     System.out.println("||======================||");
                     System.out.println("|| FINALIZAR UMA TAREFA ||");
                     System.out.println("||======================||");
-                    for (int i = 0; i < tarefas.size(); i++) {
-                        boolean f = tarefas.get(i).getFinalizada();
-                        String nome = tarefas.get(i).getAssunto();
-                        Tarefa n = tarefas.get(i);
 
-                        if (f) {
-                            System.out.print("finalizada");
-                        } else {
-                            System.out.print("não finalizada");
-                        }
+                    id = usuarioLogado.getId();
+                    listarTarefas(id);
 
-                        System.out.println("Tarefa[" + n + "]: " + nome + " " + f);
-                    }
-
-                    System.out.println("Insira o identificador da tarefa para finalizar: ");
+                    System.out.print("Insira o identificador da tarefa para finalizar: ");
                     idTarefa = sc.nextInt();
 
                     finalizarTarefa(idTarefa);
@@ -435,19 +452,9 @@ public class MySQLII {
                     System.out.println("||====================||");
                     System.out.println("|| REMOVER UMA TAREFA ||");
                     System.out.println("||====================||");
-                    for (int i = 0; i < tarefas.size(); i++) {
-                        boolean f = tarefas.get(i).getFinalizada();
-                        String nome = tarefas.get(i).getAssunto();
-                        Tarefa n = tarefas.get(i);
-
-                        if (f) {
-                            System.out.print("finalizada");
-                        } else {
-                            System.out.print("não finalizada");
-                        }
-
-                        System.out.println("Tarefa[" + n + "]: " + nome + " " + f);
-                    }
+                    
+                    id = usuarioLogado.getId();
+                    listarTarefas(id);
 
                     System.out.println("Insira o identificador da tarefa para remover: ");
                     idTarefa = sc.nextInt();
